c5cdf6bee6b9b292eeaf42d23f9cd157cf0b25e2
mostofa62 <golammostofa31188@gmail.com>
2023-06-27 22:05:40 +0600

    forget to add https in url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'standalone',
   basePath: '/app',
     env: {
-        url: 'http://nilphamaricity.com/api/',
+        url: 'https://nilphamaricity.com/api/',
         per_page:10,
         per_page_list:[10,20,50,100]
       },
