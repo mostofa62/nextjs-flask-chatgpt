550e804d08837c82c5b32e3c66f2a90a827e5666
mostofa62 <golammostofa31188@gmail.com>
2023-06-29 22:28:20 +0600

    edcoach.ai domain added

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'standalone',
   basePath: '/app',
     env: {
-        url: 'https://nilphamaricity.com/api/',
+        url: 'https://edcoach.ai/api/',
         per_page:10,
         per_page_list:[10,20,50,100]
       },
