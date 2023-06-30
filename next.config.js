e81a786deb074d31e1c92b855f8e2952d296af03
mostofa62 <golammostofa31188@gmail.com>
2023-06-30 12:10:11 +0600

    edcoach.io added

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'standalone',
   basePath: '/app',
     env: {
-        url: 'https://edcoach.ai/api/',
+        url: 'https://edcoach.io/api/',
         per_page:10,
         per_page_list:[10,20,50,100]
       },
